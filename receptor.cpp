1432ac2c8bab6d3d3cd25a5bc086602d98ab461d
Robelox22 <136749121+Robelox22@users.noreply.github.com>
2023-06-20 12:50:37 -0600

    Update receptor.cpp

--- receptor.cpp
+++ receptor.cpp
@@ -1,28 +1,43 @@
-#include <IRMP.h>
+#include <Arduino.h>
+#include <irmp.hpp>
+#include <TinyIR.h>
+#include <TinyIRSender.hpp>
+#include <TinyIRReceiver.hpp>
 
-const int RECV_PIN = PA0; // Pin de conexión del módulo receptor infrarrojo
-const int LED_PIN = PC13; // Pin para el LED
+#define RECV_PIN PA6   // Pin de recepción del receptor infrarrojo
+#define LED_PIN  PC13  // Pin del LED
 
-IRMP irrecv(RECV_PIN);
-IRMP_DATA results;
-
+IRMP_DATA irmpData;
+bool ledState;
+bool state;
 void setup() {
-  Serial.begin(9600);
-  irrecv.enableIRIn(); // Iniciar el receptor infrarrojo
-  pinMode(LED_PIN, OUTPUT); // Configurar el pin del LED como salida
+ pinMode(LED_PIN, OUTPUT);   // Configurar el pin del LED como salida
+ pinMode(RECV_PIN, INPUT);
+  irmp_init();
+  Serial.begin(115200);
+  irmpData.protocol = IRMP_NEC_PROTOCOL;
+  irmpData.address = 0x0707;
+  irmpData.command = 0x3B; // The required inverse of the 8 bit command is added by the send routine.
+  irmpData.flags = 2; // repeat frame 2 times*/
+  //irmp_add_protocol(&IRMP_NEC_PROTOCOL);  // Agregar el protocolo NEC al receptor
+  //irmp_enable();
 }
 
 void loop() {
-  if (irrecv.available()) {
-    // Si se recibió una señal infrarroja, encender el LED
-    digitalWrite(LED_PIN, HIGH);
-    irrecv.read(&results);
-    Serial.print("Valor recibido: 0x");
-    Serial.println(results.command, HEX);
-  } else {
-    // Si no se recibió una señal infrarroja, apagar el LED
-    digitalWrite(LED_PIN, LOW);
+  state = digitalRead(RECV_PIN);
+  if (irmp_get_data(&irmpData)) {
+    
+    if (state)
+      digitalWrite(LED_PIN, HIGH);
+    else
+      digitalWrite(LED_PIN, LOW);
+
+
+
+    //irmp_reset_data();  // Reiniciar el buffer de datos para recibir el próximo código IR
   }
+    delay(1000);
+  
 }
 
 
