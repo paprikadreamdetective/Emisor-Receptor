d5fb963ba301e5d0c1066040360f72766c55d499
Octavio <133156970+paprikadreamdetective@users.noreply.github.com>
2023-06-21 10:46:48 -0600

    Signal, buttons & analog input

--- emisor.cpp
+++ emisor.cpp
@@ -1,49 +1,78 @@
 #include <Arduino.h>
-#include <irmp.hpp>
-#include <TinyIR.h>
-#include <TinyIRSender.hpp>
-#include <TinyIRReceiver.hpp>
 #include <IRremote.h>
 #include <IRSend.hpp>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "SSD1306_Defs.h"
+#include <IRremote.h>
 
-IRsend irsend;
+#define SEND_PIN PA0
 
+namespace global {
+  Adafruit_SSD1306* OLED;
+  int iE, iR;
+  float vE, vR;
+  bool stateLed = true;
+  bool stateSignal = false;
+}
 
-#define LED_BUILT PC13
-#define SEND_PIN PA6
-#define BUTTON_1 PA0    // Correspondiente al boton para enviar la señal
-#define BUTTON_2 PA1
-bool OnSignal = false;
-
-//IRMP_DATA send_data;
+namespace globalSender {
+  IRsend ir_send(SEND_PIN);
+}
 
 void setup() {
-  //irmp_init();
-  pinMode(SEND_PIN, OUTPUT);
-  pinMode(LED_BUILT, OUTPUT);
-  pinMode(BUTTON_1, INPUT);
-  pinMode(BUTTON_2, INPUT);
-  //irmp_irsnd_LEDFeedback(true); // Enable send signal feedback at LED_BUILTIN
-  /*send_data.protocol = IRMP_NEC_PROTOCOL;
-  send_data.address = 0x0707;
-  send_data.command = 0x3B; // The required inverse of the 8 bit command is added by the send routine.
-  send_data.flags = 2; // repeat frame 2 times
-  //irmpData.irsnd_init();
-  Serial.begin(115200);*/
+  pinMode(PA0, OUTPUT);
+  pinMode(PA1, INPUT);
+  pinMode(PA2, INPUT);
+  pinMode(PA3, INPUT);
+  pinMode(PA4, INPUT);
+  Wire.begin(SDA, SCL);
+  global::OLED = new Adafruit_SSD1306(OLED_WIDTH, OLED_HEIGHT);
+  global::OLED->begin(SSD1306_SWITCHCAPVCC, 60);
+  global::OLED->setTextColor(SSD1306_WHITE);
 }
-// Funcon main
-void loop() {
 
-  if (digitalRead(BUTTON_1)){
-    digitalWrite(LED_BUILT, HIGH);
-    sendNEC(SEND_PIN, 0, 0x00ff, 32);
+void loop() {
+  global::OLED->clearDisplay();
+  global::OLED->setCursor(0, 0);
+  global::OLED->setTextSize(1.5);
+  //global::OLED->print("----------\n");
+  global::iE = analogRead(PA1);       // Lectura analogica del potenciometro.
+  global::vE = map(global::iE, 0, 1023, 0, 3.3);
+  global::OLED->print("- " + String(global::vE) + " V\n");
+  global::OLED->display();
+  delay(1000);
+  if(digitalRead(PA2)) // lectura del boton de envio para enviar la señal
+    global::stateSignal = !global::stateSignal;
+  if (global::stateSignal){
+    globalSender::ir_send.sendSony(0x07b, global::iE, 16);
+    global::OLED->print("- send ON:" + String(global::iE) + "\n");
+    global::OLED->print("\n");
+    global::OLED->display();
+    delay(1000);
+  }else{
+    global::OLED->print("- send OFF:" + String(global::iE) + "\n");
+    global::OLED->print("\n");
+    global::OLED->display();
     delay(1000);
   }
-
-  if (!digitalRead(BUTTON_2)){
-    sendNEC(SEND_PIN, 0, 0x00ff);
-    delay(1000);
-    sendNEC(SEND_PIN, 0, 0xff00);
-    delay(1000);
+  if(digitalRead(PA3)){  // lectura del boton de apagado
+    delay(50);
+    if(global::stateLed){
+      global::iE = 1023;
+      globalSender::ir_send.sendSony(0x07b, global::iE, 16);
+      global::OLED->print("- Led ON: " + String(global::iE) + "\n");
+      global::OLED->display();
+      global::stateLed = false;
+      delay(1000);
+    }else{
+      global::iE = 0;
+      globalSender::ir_send.sendSony(0x07b, global::iE, 16);
+      global::OLED->print("- Led OFF: " + String(global::iE) + "\n");
+      global::OLED->display();
+      global::stateLed = true;
+      delay(1000);
+    }
   }
 }
