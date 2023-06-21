9b1248168129043da636fbd41dfc8fd7240afc61
Robelox22 <136749121+Robelox22@users.noreply.github.com>
2023-06-21 12:58:12 -0600

    Update receptor.cpp
    Se recibio señal en la pantalla y en el LED

--- receptor.cpp
+++ receptor.cpp
@@ -1,43 +1,65 @@
 #include <Arduino.h>
-#include <irmp.hpp>
-#include <TinyIR.h>
-#include <TinyIRSender.hpp>
-#include <TinyIRReceiver.hpp>
+#include <IRremote.h>   // Libreria que proporciona los metodos necesarios para recibir una señal
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "SSD1306_Defs.h"
+#include <IRremote.h>
 
-#define RECV_PIN PA6   // Pin de recepción del receptor infrarrojo
-#define LED_PIN  PC13  // Pin del LED
+#define RECV_PIN A6 // Pin que recibira la señal del emisor infrarrojo (conectado a el receptor)
+#define LED A0      // Pin del led a encender
 
-IRMP_DATA irmpData;
-bool ledState;
-bool state;
-void setup() {
- pinMode(LED_PIN, OUTPUT);   // Configurar el pin del LED como salida
- pinMode(RECV_PIN, INPUT);
-  irmp_init();
-  Serial.begin(115200);
-  irmpData.protocol = IRMP_NEC_PROTOCOL;
-  irmpData.address = 0x0707;
-  irmpData.command = 0x3B; // The required inverse of the 8 bit command is added by the send routine.
-  irmpData.flags = 2; // repeat frame 2 times*/
-  //irmp_add_protocol(&IRMP_NEC_PROTOCOL);  // Agregar el protocolo NEC al receptor
-  //irmp_enable();
+// Contiene los atributos de la pantalla OLED
+namespace global {
+  Adafruit_SSD1306* OLED;
+  int iE, iR;
+  float vE, vR;
+  bool stateLed = true;
+  bool stateSignal = false;
+}
+// Campos correspondientes a al objeto que recibira la señal
+namespace globalReceiver{
+  IRrecv irrecv(RECV_PIN);
+  decode_results results;
 }
 
-void loop() {
-  state = digitalRead(RECV_PIN);
-  if (irmp_get_data(&irmpData)) {
-    
-    if (state)
-      digitalWrite(LED_PIN, HIGH);
-    else
-      digitalWrite(LED_PIN, LOW);
+void setup(){
+  pinMode(LED, OUTPUT);
+  globalReceiver::irrecv.enableIRIn();
+  Wire.begin(SDA, SCL);
+  global::OLED = new Adafruit_SSD1306(OLED_WIDTH, OLED_HEIGHT);
+  global::OLED->begin(SSD1306_SWITCHCAPVCC, 60);
+  global::OLED->setTextColor(SSD1306_WHITE);
+}
+
+void loop(){
 
 
+    static uint8_t command = 0;
+    static float V = 0;
+    char buffer[10];
 
-    //irmp_reset_data();  // Reiniciar el buffer de datos para recibir el próximo código IR
-  }
+    global::OLED->clearDisplay();
+    global::OLED->setCursor(0, 0);
+    global::OLED->setTextSize(1.5);
+    global::vE = map(globalReceiver::irrecv.decodedIRData.command, 0, 1023, 0, 3.3);
+    global::OLED->print("- " + String(global::vE) + " V\n");
+    global::OLED->display();
     delay(1000);
-  
+
+    //if (globalReceiver::irrecv.decodedIRData.command)
+    if (globalReceiver::irrecv.decode()){
+      if (globalReceiver::irrecv.decodedIRData.command > 9) {
+        command = globalReceiver::irrecv.decodedIRData.command;
+      }
+    }
+    /*
+    digitalWrite(LED, state);*/
+    digitalWrite(LED, HIGH);  // Enciende el LED
+    delay(1000);                   // Espera 1 segundo
+    digitalWrite(LED, LOW);   // Apaga el LED
+    globalReceiver::irrecv.resume();
+  delay(200);
 }
 
 
